62f12ddf3f70d08c0f12c91bd5c181377bcfd969
James Phong Duong <james.duong93@gmail.com>
2019-03-22 16:49:28 +1100

    banner position changed to left

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
 import swal from 'sweetalert';
+import { Container } from 'semantic-ui-react';
 
 // onload as we have to wait for the dom to be created first
 window.onload = () => {
@@ -25,26 +26,32 @@ window.onload = () => {
 const Banner = () => {
   return (
     <div className='banner'>
-      <h1> James Duong </h1>
-      <h3> web developer </h3>
-      <ul>
-        <li>
-          <a href='https://github.com/jamesphongduong' target='_blank'>
-            <Icon bordered name='github alternate' />
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://www.linkedin.com/in/james-phong-duong-9168a89a/'
-            target='_blank'
-          >
-            <Icon bordered name='linkedin' />
-          </a>
-        </li>
-        <li>
-          <Icon bordered name='mail' id='email' />
-        </li>
-      </ul>
+      <Container>
+        <h1> JAMES DUONG. </h1>
+        <h3> web developer </h3>
+        <ul className='plugs'>
+          <li>
+            <a
+              href='https://github.com/jamesphongduong'
+              target='_blank'
+              className='test'
+            >
+              <Icon size='large' bordered name='github alternate' />
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.linkedin.com/in/james-phong-duong-9168a89a/'
+              target='_blank'
+            >
+              <Icon size='large' bordered name='linkedin' />
+            </a>
+          </li>
+          <li>
+            <Icon size='large' bordered name='mail' id='email' />
+          </li>
+        </ul>
+      </Container>
     </div>
   );
 };
