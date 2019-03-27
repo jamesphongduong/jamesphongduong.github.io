6580828ba07a8a5df5341d1d90e1b2992c8e3a01
James Phong Duong <james.duong93@gmail.com>
2019-03-28 00:33:24 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio_react/precache-manifest.50271fc90abea3b82cfe5214ed187a26.js"
+  "/portfolio_react/precache-manifest.a345a93d032f734db74e807a29dbf5c0.js"
 );
 
 workbox.clientsClaim();
