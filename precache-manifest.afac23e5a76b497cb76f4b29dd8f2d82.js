b89f7359b49a483fb5892e99dc60c7f1ebf52b3b
James Phong Duong <james.duong93@gmail.com>
2019-03-27 22:31:47 +1100

    Updates

---
+++ precache-manifest.afac23e5a76b497cb76f4b29dd8f2d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63bf94321b81b506aa29",
+    "url": "/portfolio_react/static/js/runtime~main.63bf9432.js"
+  },
+  {
+    "revision": "bbdec257ec020a6ff2e8",
+    "url": "/portfolio_react/static/js/main.bbdec257.chunk.js"
+  },
+  {
+    "revision": "003c93d26f12d41965dd",
+    "url": "/portfolio_react/static/js/2.003c93d2.chunk.js"
+  },
+  {
+    "revision": "bbdec257ec020a6ff2e8",
+    "url": "/portfolio_react/static/css/main.19f8459c.chunk.css"
+  },
+  {
+    "revision": "f25d643e6be243344f4088e105b08436",
+    "url": "/portfolio_react/index.html"
+  }
+];
